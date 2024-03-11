d56cb41049752fdcb45e4c0999fb45a2c64091a8
Enzo Sandre <enzo@enzosandre.fr>
2024-03-11 09:54:51 +0100

    Définition du prix unitaire

--- DemoTCR.Test/ImprimanteTest.cs
+++ DemoTCR.Test/ImprimanteTest.cs
@@ -11,5 +11,18 @@ namespace DemoTCR.Test
             var prix = new decimal(0).ToString(Imprimante.FormatPrix);
             Assert.Equal(Imprimante.PrixTotal + prix, ticket);
         }
+
+        [Fact]
+        public void TestPrixTotalFixe()
+        {
+            var prixUnitaire = new decimal(1);
+
+            var imprimante = new Imprimante();
+            imprimante.DéfinirPrixUnitaire(prixUnitaire);
+            var ticket = imprimante.Imprimer();
+
+            var prix = prixUnitaire.ToString(Imprimante.FormatPrix);
+            Assert.Equal(Imprimante.PrixTotal + prix, ticket);
+        }
     }
 }
--- DemoTCR/Imprimante.cs
+++ DemoTCR/Imprimante.cs
@@ -5,9 +5,16 @@ public class Imprimante
     public const string PrixTotal = "Prix Total : ";
     public const string FormatPrix = "C";
 
+    private decimal _prixUnitaire = new (0);
+
     public string Imprimer()
     {
-        var prixTotal = new decimal(0).ToString(FormatPrix);
+        var prixTotal = _prixUnitaire.ToString(FormatPrix);
         return PrixTotal + prixTotal;
+    }
+
+    public void DéfinirPrixUnitaire(decimal prixUnitaire)
+    {
+        _prixUnitaire = prixUnitaire;
     }
 }
